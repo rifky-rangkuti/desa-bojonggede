fda982c75677c3a3274134cf4419ea3024bb8670
Muhammad Rifky <rifky@virtualspirit.me>
2022-12-30 01:47:51 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import Header from '../components/Header.vue'
+import Banner from '../components/Banner.vue'
+import Sidebar from '../components/Sidebar.vue'
+</script>
+
+<template>
+  <Header/>
+  <Banner/>
+  <main class="flex">
+    <Sidebar/>
+    <div class="flex-1 p-10">
+
+
+      <p class="text-3xl font-semibold">Visi & Misi Desa</p>
+      <p class="text-lg font-semibold mt-4">Visi</p>
+      <p>Membangun desa digital</p>
+      <p class="text-lg font-semibold mt-4">Misi</p>
+      <ul>
+        <li>Misi 1 desa masing-masing</li>
+        <li>Misi 2 desa masing-masing</li>
+        <li>Misi 3 desa masing-masing</li>
+        <li>Misi 4 desa masing-masing</li>
+      </ul>
+
+      <p class="text-3xl font-semibold my-4">Struktur Desa</p>
+      <img src="@/assets/struktur-desa-bojonggede.png" alt=""/>
+    </div>
+  </main>
+</template>
+
+<style>
+  body{
+    background: #f1f1f1;
+  }
+</style>
